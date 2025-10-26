e6600e980de8b56fc05ff3814d17e0afc0c91d3e
Abdullah Rufai <abdullahrufai72@gmail.com>
2025-10-27 00:49:23 +0100

    Update init.sql

--- scripts/Full_load/bronze/init.sql
+++ scripts/Full_load/bronze/init.sql
@@ -2,6 +2,14 @@
 
 This script creates the database and schema objects for the three layers of the ETL process.
 */
+
+
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'Nyc_DataWarehouse')
+BEGIN
+    ALTER DATABASE Nyc_DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE Nyc_DataWarehouse;
+END;
+
 CREATE DATABASE Nyc_DataWarehouse;
 GO
 
