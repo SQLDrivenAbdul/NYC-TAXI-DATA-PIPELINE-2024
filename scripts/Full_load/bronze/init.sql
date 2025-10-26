d28494967aabbf8852a78e16db035d8327cf14d6
Abdullah Rufai <abdullahrufai72@gmail.com>
2025-10-27 00:11:38 +0100

    Create Nyc_DataWarehouse and schemas for ETL
    This script sets up the database and schemas for the ETL process.

--- scripts/Full_load/bronze/init.sql
+++ scripts/Full_load/bronze/init.sql
@@ -1,1 +1,22 @@
-a
+/*
+
+This script creates the database and schema objects for the three layers of the ETL process.
+*/
+CREATE DATABASE Nyc_DataWarehouse;
+GO
+
+
+USE Nyc_DataWarehouse;
+GO
+
+
+CREATE SCHEMA bronze;
+GO
+
+
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
