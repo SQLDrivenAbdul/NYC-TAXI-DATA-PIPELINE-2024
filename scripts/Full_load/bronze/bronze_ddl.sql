fffd4949f83ebd69f8e04e2c7aeff0d7a13319eb
Abdullah Rufai <abdullahrufai72@gmail.com>
2025-10-27 01:00:18 +0100

    Create bronze.yellow_taxi table with structure
    Added comments and created table structure for yellow_taxi.

--- scripts/Full_load/bronze/bronze_ddl.sql
+++ scripts/Full_load/bronze/bronze_ddl.sql
@@ -1,1 +1,30 @@
-a
+/*
+At every run of this script, the table is dropped and recreated.
+*/
+
+
+-- creating the table structure
+ IF OBJECT_ID('bronze.yellow_taxi', 'U') IS NOT NULL
+	DROP TABLE bronze.yellow_taxi;
+CREATE TABLE bronze.yellow_taxi
+(
+VendorID INT,
+tpep_pickup_datetime VARCHAR(30),
+tpep_dropoff_datetime VARCHAR(30),
+passenger_count INT,
+trip_distance FLOAT,
+RatecodeID INT,
+store_and_fwd_lag CHAR(10),
+PULocationID INT,
+DULocationID INT,
+payment_type INT,
+fare_amount FLOAT,
+extra FLOAT,
+mta_tax FLOAT,
+tip_amount FLOAT,
+tolls_amount FLOAT,
+improvement_surcharge FLOAT,
+total_amount FLOAT,
+congestion_surcharge FLOAT,
+airport_fee FLOAT
+)
