5d98d727364ca1a16d2268a606eb2444b8a8548f
Abdullah Rufai <abdullahrufai72@gmail.com>
2025-11-08 10:45:27 +0100

    Update load_bronze.sql

--- scripts/Full_load/bronze/load_bronze.sql
+++ scripts/Full_load/bronze/load_bronze.sql
@@ -1,52 +1,135 @@
 /*
-
-
-At every run, the bronze.yellow_taxi table is truncated and reloaded
-
-For easy execution, the code is wrapped in a stored procedure AS bronze.load_nycbronze.
-To use the stored procedure, you simply run  ``EXEC bronze.load_nycbronze`` in your management system.
-
-Case when the procedure fails to load the data, the script has been designed to  report  some important information about the error encountered for easy debugging.
+The stored procedure is designed to load all of 2024 data at once into the raw table.
+To use the procedure, run EXEC bronze.load_nycbronze in your management system.
 */
-EXEC bronze.load_nycbronze;
-
-
-CREATE OR ALTER PROCEDURE bronze.load_nycbronze AS
+CREATE PROCEDURE bronze.load_nycbronze
+AS
 BEGIN
-  
-BEGIN TRY
-DECLARE @load_start_time DATETIME , @load_end_time DATETIME
+	DECLARE @load_start_time DATETIME , @load_end_time DATETIME
 
+SET @load_start_time = GETDATE()
 
-SET @load_start_time = GETDATE() -- get the exact time the file starts loading
-
--- truncates table
-TRUNCATE TABLE bronze.yellow_taxi;
-
--- loading the bronze layer
+PRINT 'January data'
 BULK INSERT bronze.yellow_taxi
-FROM 'C:\Users\USER\OneDrive\Desktop\NYC_ETL_PRROJECT FILE\combined_full_load.csv'
-WITH 
-( 
+FROM 'C:\Users\USER\OneDrive\Desktop\NYC_ETL_PRROJECT FILE\yellow_tripdata_2024-01.csv'
+WITH
+(
+FIRSTROW = 2,
+FIELDTERMINATOR = ',',
+TABLOCK
+);
+
+PRINT 'February data'
+BULK INSERT bronze.yellow_taxi
+FROM 'C:\Users\USER\OneDrive\Desktop\NYC_ETL_PRROJECT FILE\yellow_tripdata_2024-02.csv'
+WITH
+(
+FIRSTROW = 2,
+FIELDTERMINATOR = ',',
+TABLOCK
+);
+
+PRINT 'March data'
+BULK INSERT bronze.yellow_taxi
+FROM 'C:\Users\USER\OneDrive\Desktop\NYC_ETL_PRROJECT FILE\yellow_tripdata_2024-03.csv'
+WITH
+(
+FIRSTROW = 2,
+FIELDTERMINATOR = ',',
+TABLOCK
+);
+
+PRINT 'April data'
+BULK INSERT bronze.yellow_taxi
+FROM 'C:\Users\USER\OneDrive\Desktop\NYC_ETL_PRROJECT FILE\yellow_tripdata_2024-04.csv'
+WITH
+(
+FIRSTROW = 2,
+FIELDTERMINATOR = ',',
+TABLOCK
+);
+
+PRINT 'May data'
+BULK INSERT bronze.yellow_taxi
+FROM 'C:\Users\USER\OneDrive\Desktop\NYC_ETL_PRROJECT FILE\yellow_tripdata_2024-05.csv'
+WITH
+(
+FIRSTROW = 2,
+FIELDTERMINATOR = ',',
+TABLOCK
+);
+
+PRINT 'June data'
+BULK INSERT bronze.yellow_taxi
+FROM 'C:\Users\USER\OneDrive\Desktop\NYC_ETL_PRROJECT FILE\yellow_tripdata_2024-06.csv'
+WITH
+(
+FIRSTROW = 2,
+FIELDTERMINATOR = ',',
+TABLOCK
+);
+
+PRINT 'July data'
+BULK INSERT bronze.yellow_taxi
+FROM 'C:\Users\USER\OneDrive\Desktop\NYC_ETL_PRROJECT FILE\yellow_tripdata_2024-07.csv'
+WITH
+(
+FIRSTROW = 2,
+FIELDTERMINATOR = ',',
+TABLOCK
+);
+
+PRINT 'August data'
+BULK INSERT bronze.yellow_taxi
+FROM 'C:\Users\USER\OneDrive\Desktop\NYC_ETL_PRROJECT FILE\yellow_tripdata_2024-08.csv'
+WITH
+(
+FIRSTROW = 2,
+FIELDTERMINATOR = ',',
+TABLOCK
+);
+
+PRINT 'September data'
+BULK INSERT bronze.yellow_taxi
+FROM 'C:\Users\USER\OneDrive\Desktop\NYC_ETL_PRROJECT FILE\yellow_tripdata_2024-09.csv'
+WITH
+(
+FIRSTROW = 2,
+FIELDTERMINATOR = ',',
+TABLOCK
+);
+
+PRINT 'October data'
+BULK INSERT bronze.yellow_taxi
+FROM 'C:\Users\USER\OneDrive\Desktop\NYC_ETL_PRROJECT FILE\yellow_tripdata_2024-10.csv'
+WITH
+(
+FIRSTROW = 2,
+FIELDTERMINATOR = ',',
+TABLOCK
+);
+
+PRINT 'November data'
+BULK INSERT bronze.yellow_taxi
+FROM 'C:\Users\USER\OneDrive\Desktop\NYC_ETL_PRROJECT FILE\yellow_tripdata_2024-11.csv'
+WITH
+(
+FIRSTROW = 2,
+FIELDTERMINATOR = ',',
+TABLOCK
+);
+
+PRINT 'December data'
+BULK INSERT bronze.yellow_taxi
+FROM 'C:\Users\USER\OneDrive\Desktop\NYC_ETL_PRROJECT FILE\yellow_tripdata_2024-12.csv'
+WITH
+(
 FIRSTROW = 2,
 FIELDTERMINATOR = ',',
 TABLOCK
 )
-SET @load_end_time = GETDATE(); -- when it's done 
-
-PRINT '---------------------------------------------------------------'
-PRINT 'Load time: ' + CAST(DATEDIFF(SECOND,@load_start_time,@load_end_time) AS VARCHAR) + ' seconds'
-PRINT '---------------------------------------------------------------'
-END TRY
-
-BEGIN CATCH
-PRINT '---------------------------------------------------------------'
-PRINT 'Error message: ' + CAST(ERROR_MESSAGE() AS VARCHAR)
-PRINT 'Error message: ' + CAST(ERROR_STATE() AS VARCHAR)
-PRINT 'Error message: ' + CAST(ERROR_NUMBER() AS VARCHAR)
-PRINT '---------------------------------------------------------------'
-END CATCH
-
-
 END
 
+SET @load_end_time = GETDATE()
+
+PRINT 'Load_time: ' + CAST(DATEDIFF(SECOND,@load_end_time,@load_start_time)AS VARCHAR) + 'seconds'
